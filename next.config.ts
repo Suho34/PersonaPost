b1d77a8192eede28ddebf00256906f9d1d0bf4d5
Suho34 <suhaibnisar01@gmail.com>
2025-12-10 18:55:56 +0530

    fix: downgrade chromium to v133 and cleanup config to fix Vercel deploy

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   serverExternalPackages: ["puppeteer-core", "@sparticuz/chromium"],
+
 };
 
 export default nextConfig;
